6fa0c26913881af8717ceb245c95e4d2a1c8c80b
wanglei <1101630145@qq.com>
2018-05-16 15:28:27 +0800

    提交

--- src/main/java/com/example/demo/service/PayService.java
+++ src/main/java/com/example/demo/service/PayService.java
@@ -3,9 +3,11 @@ package com.example.demo.service;
 import com.example.demo.DTO.OrderDTO;
 
 public interface PayService {
-//    PayResponse create(OrderDTO orderDTO);
+
+    void create(OrderDTO orderDTO);
+//      PayResponse create(OrderDTO orderDTO);
 //
-//    PayResponse notify(String notifyData);
+//      PayResponse notify(String notifyData);
 //
 //    RefundResponse refund(OrderDTO orderDTO);
 }
